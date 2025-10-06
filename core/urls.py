b8dd60d7b840ff0611fcfe93832359ba0c081809
Peter Sully <petersully@Peters-MacBook-Air.local>
2025-10-06 21:43:57 +1000

    Added dashboard & updated URL links

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import home, upload_doc
+from . import views
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("upload/", upload_doc, name="upload"),
+    path("", views.dashboard, name="dashboard"),
+    path("prospects/", views.prospects, name="prospects"),
+    path("drillholes/", views.drillholes, name="drillholes"),
+    path("tenements/", views.tenements, name="tenements"),
+    path("documents/", views.documents, name="documents"),
+    path("ai-insights/", views.ai_insights, name="ai_insights"),
+
+    path("upload/", views.upload_doc, name="upload"),
 ]
